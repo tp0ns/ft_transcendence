a6ac7a86a3c0e7b4b10dfa06f22009832720f26b
tp0ns <tpons@student.42.fr>
2022-07-07 12:00:53 +0200

    Gets rid of node modules in root directory

---
+++ backend/src/auth/auth.strategy.ts
@@ -0,0 +1,24 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { Strategy } from 'passport-oauth2';
+
+@Injectable()
+export class schoolStrategy extends PassportStrategy(Strategy, '42')
+{
+	constructor() {
+		super({
+			clientID: process.env.FORTYTWO_APP_ID,
+    	clientSecret: process.env.FORTYTWO_APP_SECRET,
+   		callbackURL: 'qwe' ,
+			// authorizationURL: null,
+			// tokenURL        : null,
+			// scope           : null,
+		});
+	}
+
+	async validate(
+		accessToken: string,
+	): Promise<any> {
+
+	}
+}
