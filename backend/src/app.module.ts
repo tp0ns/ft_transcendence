53942207696ee068d147ba300bb829a033fe46e6
tp0ns <tpons@student.42.fr>
2022-07-07 12:13:13 +0200

    Auth module and strategy

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeOrmConfig } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [UserModule, TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { schoolStrategy } from './auth.strategy';
+
+@Module({
+	imports:[PassportModule.register(schoolStrategy)]
+})
+export class AuthModule {}
