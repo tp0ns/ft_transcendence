ce75cd3c5e4bc92d84705eb5501bd05ad9e7ef86
tp0ns <tpons@student.42.fr>
2022-07-11 20:42:44 +0200

    Comments for OAuth42 and JWT

--- backend/src/auth/jwt/jwt.guard.ts
+++ backend/src/auth/jwt/jwt.guard.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
+/**
+ * C'est grace au module passport-jwt que l'on a pas besoin de specifier le comportement du AuthGuard.
+ * passport-jwt le fait pour nous, on renseigne simplement qu'on utilise le module / la stratégie 'jwt'.
+ */
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {}
