847cd87e216913d2df24e62e74e561c16649b0b4
tp0ns <tpons@student.42.fr>
2022-07-12 11:43:31 +0200

    401 exceptions now redirects to login page.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { UnauthorizedExceptionFilter } from './unauthorized-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+	app.useGlobalFilters(new UnauthorizedExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
