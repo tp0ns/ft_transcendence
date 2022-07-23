9c0ef668643d8fd77d6d979a034a70bd25cdae38
Clara Boucher <clbouche@student.42.fr>
2022-07-23 11:15:37 +0200

    button for chat in progress

--- frontend/node_modules/@volar/vue-typescript/out/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from './utils/sourceMaps';
-export * from './utils/string';
-export * from './sourceFile';
-export * from './documentRegistry';
-export * from './types';
-export * from './lsContext';
-export * as localTypes from './utils/localTypes';
-export * as tsShared from './utils/ts';
