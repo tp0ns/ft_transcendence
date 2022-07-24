6188163d8aee432bf716f69f25ad83d3b4d8fc6a
Clara Boucher <clbouche@student.42.fr>
2022-07-24 11:51:38 +0200

    css clean

--- frontend/src/views/Chat.vue
+++ frontend/src/views/Chat.vue
@@ -2,7 +2,13 @@
 	
 <div>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Nestjs SocketIO</title>
+    <link rel="stylesheet" href="styles.css">
+    <!-- <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script> -->
+    <!-- <script src="https://cdn.socket.io/4.3.2/socket.io.min.js" integrity="sha384-KAZ4DtjNhLChOB/hxXuKqhMLYvx3b5MlT55xPEiNmREKRzeEm+RVPlTnAn0ajQNs" crossorigin="anonymous"></script> -->
 </head>
     <div id="app" class="container">
             <div class="row">
