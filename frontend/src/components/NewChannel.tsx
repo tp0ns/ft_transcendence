e24c243d705797f9012eb5fb5ec21653bf1a0efd
tp0ns <tpons@student.42.fr>
2022-08-02 10:18:18 +0200

    Frontend setup for auth front

--- frontend/src/components/NewChannel.tsx
+++ frontend/src/components/NewChannel.tsx
@@ -2,80 +2,80 @@ import { useRef, useState } from "react";
 import ChannelProp from "../interfaces/Channel.interface";
 
 import Card from "../ui/Card";
-import classes from "./NewChannelForm.module.css";
+// import classes from "./NewChannelForm.module.css";
 
 const NewChannelForm: React.FC<{
-  sendChan: (channelData: ChannelProp) => void;
+	sendChan: (channelData: ChannelProp) => void;
 }> = (props: any) => {
-  const titleInputRef = useRef<HTMLInputElement>(null);
-  const passwordInputRef = useRef<HTMLInputElement>(null);
-  const [privateChan, setPrivateChan] = useState(false);
-  const [protectedChan, setProtectedChan] = useState(false);
+	const titleInputRef = useRef<HTMLInputElement>(null);
+	const passwordInputRef = useRef<HTMLInputElement>(null);
+	const [privateChan, setPrivateChan] = useState(false);
+	const [protectedChan, setProtectedChan] = useState(false);
 
-  function submitHandler(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+	function submitHandler(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
 
-    const enteredTitle: string = titleInputRef.current!.value;
-    let enteredPassword: string = "123";
-    if (passwordInputRef.current) {
-      enteredPassword = passwordInputRef.current!.value;
-    }
+		const enteredTitle: string = titleInputRef.current!.value;
+		let enteredPassword: string = "123";
+		if (passwordInputRef.current) {
+			enteredPassword = passwordInputRef.current!.value;
+		}
 
-    const channelData = {
-      id: "",
-      title: enteredTitle,
-      password: enteredPassword,
-      private: privateChan,
-      protected: protectedChan,
-    };
-    // channelsCtx.addChannel(channelData);
-    props.sendChan(channelData);
-  }
+		const channelData = {
+			id: "",
+			title: enteredTitle,
+			password: enteredPassword,
+			private: privateChan,
+			protected: protectedChan,
+		};
+		// channelsCtx.addChannel(channelData);
+		props.sendChan(channelData);
+	}
 
-  function handlePrivate(event: any) {
-    event.preventDefault();
-    setPrivateChan((prevState) => {
-      return !prevState;
-    });
-  }
+	function handlePrivate(event: any) {
+		event.preventDefault();
+		setPrivateChan((prevState) => {
+			return !prevState;
+		});
+	}
 
-  function handleProtected(event: any) {
-    event.preventDefault();
-    setProtectedChan((prevState) => {
-      return !prevState;
-    });
-  }
+	function handleProtected(event: any) {
+		event.preventDefault();
+		setProtectedChan((prevState) => {
+			return !prevState;
+		});
+	}
 
-  return (
-    <Card>
-      <form className={classes.form} onSubmit={submitHandler}>
-        <div className={classes.control}>
-          <label htmlFor="title">Channel Title</label>
-          <input type="text" required id="title" ref={titleInputRef} />
-        </div>
-        <div className={classes.control}>
-          <button onClick={handleProtected}>
-            {!protectedChan ? "Protect" : "Unprotect"}
-          </button>
-        </div>
-        {protectedChan ? (
-          <div className={classes.control}>
-            <label htmlFor="image">Channel Password</label>
-            <input type="text" required id="image" ref={passwordInputRef} />
-          </div>
-        ) : null}
-        <div className={classes.control}>
-          <button onClick={handlePrivate}>
-            {!privateChan ? "Make private" : "Make public"}
-          </button>
-        </div>
+	return (
+		<Card>
+			<form onSubmit={submitHandler}>
+				<div>
+					<label htmlFor="title">Channel Title</label>
+					<input type="text" required id="title" ref={titleInputRef} />
+				</div>
+				<div>
+					<button onClick={handleProtected}>
+						{!protectedChan ? "Protect" : "Unprotect"}
+					</button>
+				</div>
+				{protectedChan ? (
+					<div>
+						<label htmlFor="image">Channel Password</label>
+						<input type="text" required id="image" ref={passwordInputRef} />
+					</div>
+				) : null}
+				<div>
+					<button onClick={handlePrivate}>
+						{!privateChan ? "Make private" : "Make public"}
+					</button>
+				</div>
 
-        <div className={classes.actions}>
-          <button>Add Channel</button>
-        </div>
-      </form>
-    </Card>
-  );
+				<div>
+					<button>Add Channel</button>
+				</div>
+			</form>
+		</Card>
+	);
 };
 
 export default NewChannelForm;
