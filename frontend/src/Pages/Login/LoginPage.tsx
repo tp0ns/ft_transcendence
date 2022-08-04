c7c8ae4edb30a13a12340d83a44163f460041c18
tp0ns <tpons@student.42.fr>
2022-08-04 11:04:52 +0200

    Login Page css ok

--- frontend/src/Pages/Login/LoginPage.tsx
+++ frontend/src/Pages/Login/LoginPage.tsx
@@ -17,17 +17,13 @@ const LoginPage = () => {
 	return (
 		<div className={classes.login}>
 			<div className={classes.login_logo}>PONG</div>
-			<div className={classes.login_list}>
-				<a
-					href="http://localhost/backend/auth/login"
-					className={classes.login_school}
-				>
-					<div className={classes.login_school_button}>
-						<div>Connect with</div>
-						<img src="/42_Logo.png" alt="42 school logo" />
-					</div>
-				</a>
-			</div>
+			<a
+				href="http://localhost/backend/auth/login"
+				className={classes.login_school}
+			>
+				<p>Connect with</p>
+				<img src="/42_Logo.png" alt="42 school logo" />
+			</a>
 		</div>
 	);
 };
