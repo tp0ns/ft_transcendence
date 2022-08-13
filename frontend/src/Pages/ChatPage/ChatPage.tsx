c007a71a080a1863571fe260f5daa041ea172db1
tp0ns <tpons@student.42.fr>
2022-08-13 15:22:45 +0200

    ChatPage Logic and use of Chat context

--- frontend/src/Pages/ChatPage/ChatPage.tsx
+++ frontend/src/Pages/ChatPage/ChatPage.tsx
@@ -1,15 +1,12 @@
 import { useEffect, useState } from "react";
-import ChannelsList from "../../components/channel/ChannelsLists";
-import NewChannelForm from "../../components/channel/NewChannel";
 import ChannelProp from "../../interfaces/Channel.interface";
-import OpenedChannel from "../../components/channel/OpenedChannel";
-import Settings from "../../components/channel/Settings";
 import { socket } from "../../App";
 import classes from "./ChatPage.module.css";
 import React from "react";
 import NavBar from "../../components/NavBar/NavBar";
-import ChannelMembersList from "../../components/channel/ChannelMembersList";
 import { ChatContextProvider } from "../../context/chat-context";
+import ChanList from "../../components/chat/ChanList/ChanList";
+import ChanContent from "../../components/chat/ChanContent";
 
 function ChatPage() {
 	const [newChannel, setNewChannel] = useState(false);
@@ -93,15 +90,14 @@ function ChatPage() {
 	}, [channelSettings]);
 
 	return (
-		<React.Fragment>
-			<NavBar />
+		<NavBar>
 			<ChatContextProvider>
 				<div className={classes.layout}>
-					<div className={classes.channel_list}>list</div>
-					<div className={classes.channel_content}>content</div>
+					<ChanList />
+					<ChanContent />
 				</div>
 			</ChatContextProvider>
-		</React.Fragment>
+		</NavBar>
 	);
 }
 
