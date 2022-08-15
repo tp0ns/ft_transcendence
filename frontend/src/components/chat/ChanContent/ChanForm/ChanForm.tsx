e443dff73742c0bbe58390ef73277d865491acc3
tp0ns <tpons@student.42.fr>
2022-08-15 15:49:51 +0200

    ChanList is now scrollable

--- frontend/src/components/chat/ChanContent/ChanForm/ChanForm.tsx
+++ frontend/src/components/chat/ChanContent/ChanForm/ChanForm.tsx
@@ -15,7 +15,7 @@ function ChanForm() {
 		const newChan = {
 			id: "",
 			title: name.current?.value,
-			private: !privacy.current?.checked,
+			private: privacy.current?.checked,
 			protected: protection.current?.checked,
 			password: password.current?.value ? password.current!.value : "",
 		};
@@ -34,7 +34,7 @@ function ChanForm() {
 		>
 			<label htmlFor="name">Channel Name</label>
 			<input type="text" id="name" ref={name} />
-			<label htmlFor="privacy">Public</label>
+			<label htmlFor="privacy">Private</label>
 			<input type="checkbox" id="privacy" ref={privacy} />
 			<label htmlFor="protection">Protect with password</label>
 			<input
--- frontend/src/components/chat/ChanList/Chan/Chan.tsx
+++ frontend/src/components/chat/ChanList/Chan/Chan.tsx
@@ -1,12 +1,14 @@
 import { useContext } from "react";
 import ChatContext from "../../../../context/chat-context";
 import ChannelInterface from "../../../../interfaces/Channel.interface";
+import classes from "./Chan.module.css";
 
 const Chan: React.FC<{ key: string; chan: ChannelInterface }> = (props) => {
 	const ctx = useContext(ChatContext);
 
 	return (
 		<button
+			className={classes.chan_item}
 			onClick={() => {
 				ctx.changeActiveChan(props.chan.title);
 			}}
