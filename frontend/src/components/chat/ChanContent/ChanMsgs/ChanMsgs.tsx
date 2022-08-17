a7e8794bab045311c36d8c8119c90f52b651674b
tp0ns <tpons@student.42.fr>
2022-08-17 20:05:08 +0200

    fixes import

--- frontend/src/components/chat/ChanContent/ChanMsgs/ChanMsgs.tsx
+++ frontend/src/components/chat/ChanContent/ChanMsgs/ChanMsgs.tsx
@@ -1,6 +1,4 @@
-import jwtDecode, { JwtPayload } from "jwt-decode";
 import { useContext, useEffect, useRef, useState } from "react";
-import { useCookies } from "react-cookie";
 import { socket } from "../../../../App";
 import ChatContext from "../../../../context/chat-context";
 import ChannelInterface from "../../../../interfaces/Channel.interface";
