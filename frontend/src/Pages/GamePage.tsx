e1f2cf83db082fc4e7d0e44b0546a9c68320c7ab
VincentBaron <vincentbaron1996@gmail.com>
2022-09-16 16:34:39 +0200

    winner page finnished

--- frontend/src/Pages/GamePage.tsx
+++ frontend/src/Pages/GamePage.tsx
@@ -1,15 +1,26 @@
 import React, { useState } from "react";
+import { socket } from "../App";
+import EndScreen from "../components/game/EndScreen";
 import GameScreen from "../components/game/GameScreen";
 import HomeScreen from "../components/game/HomeScreen";
 import Layout from "../components/Layout/Layout";
 
 const GamePage = () => {
 	const [gameState, setGameState] = useState<string>("noGame");
+	const [winner, setWinner] = useState<string>("");
 
 	const handleGameType = (state: string) => {
 		setGameState(state);
-
 	}
+
+	socket.on('victoryOf', (winnerSock) => {
+		console.log("entered Victory of: ", winnerSock)
+		if (gameState === "matchGame")
+			setWinner(winnerSock.username);
+		else if (gameState === "localGame")
+			setWinner("");
+		setGameState("endGame");
+	});
 
 
 	return (
@@ -17,7 +28,7 @@ const GamePage = () => {
 			<div>
 				{gameState === "noGame" ? <HomeScreen handleGame={handleGameType} /> : null}
 				{gameState === ("localGame" || "matchGame") ? <GameScreen gameType={gameState} /> : null}
-				{/* {gameState === "endGame" ? <EndScreen /> : null} */}
+				{gameState === "endGame" ? <EndScreen winner={winner} handleGame={handleGameType} /> : null}
 			</div>
 		</Layout>
 	);
--- frontend/src/components/game/HomeScreen.tsx
+++ frontend/src/components/game/HomeScreen.tsx
@@ -1,37 +1,4 @@
 import classes from "./GameScreen.module.css";
-import { socket } from "../../App";
-import { useEffect, useRef } from "react";
-
-let ballPosition = {
-  x: 0,
-  y: 0,
-  radius: 10,
-  startAngle: 0,
-  speedx: 5,
-  speedy: 0,
-  goRight: 0,
-  p1Touches: 0,
-  p2Touches: 0,
-  isMoving: false,
-};
-let leftPadPosition = {
-  x: 0,
-  y: 50,
-  w: 20,
-  h: 100,
-  speed: 5,
-};
-
-let rightPadPosition = {
-  x: 620,
-  y: 200,
-  w: 20,
-  h: 100,
-  speed: 20,
-};
-
-let player1Score: number = 0;
-let player2Score: number = 0;
 
 const HomeScreen: React.FC<{
   handleGame: (state: string) => void
