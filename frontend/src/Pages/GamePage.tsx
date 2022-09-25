c88b6de55d90d26c56fa1be5a00828289493c3af
tp0ns <tpons@student.42.fr>
2022-09-25 17:00:05 +0200

    CSS for badges

--- frontend/src/Pages/GamePage.tsx
+++ frontend/src/Pages/GamePage.tsx
@@ -1,49 +1,105 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 import { socket } from "../App";
 import GameCanvas from "../components/game/GameCanvas";
 import { Game } from "../components/game/interfaces/game.interfaces";
 import Layout from "../components/Layout/Layout";
-import classes from "./GamePage.module.css"
+import classes from "./GamePage.module.css";
 
 const GamePage = () => {
 	const [game, setGame] = useState<Game>();
-	const autoFocusRef = useRef<HTMLDivElement>(null)
+	const autoFocusRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
-		if (autoFocusRef.current)
-			autoFocusRef.current.focus()
-		socket.emit('getMyGame');
-		socket.on('updatedGame', (updatedGame) => {
-			setGame(updatedGame)
-		})
-	}, [])
+		if (autoFocusRef.current) autoFocusRef.current.focus();
+		socket.emit("getMyGame");
+		socket.on("updatedGame", (updatedGame) => {
+			setGame(updatedGame);
+		});
+	}, []);
 
 	const handleKeyDown = (event: any) => {
 		if (game) {
 			if (event.key === "w")
-				socket.emit("movePad", { direction: "up", roomId: game.id, type: "online" });
+				socket.emit("movePad", {
+					direction: "up",
+					roomId: game.id,
+					type: "online",
+				});
 			if (event.key === "s")
-				socket.emit("movePad", { direction: "down", roomId: game.id, type: "online" });
-			if (game.type === 'local') {
+				socket.emit("movePad", {
+					direction: "down",
+					roomId: game.id,
+					type: "online",
+				});
+			if (game.type === "local") {
 				if (event.keyCode === 38)
-					socket.emit("movePad", { direction: "up", roomId: game.id, type: "local" });
+					socket.emit("movePad", {
+						direction: "up",
+						roomId: game.id,
+						type: "local",
+					});
 				if (event.keyCode === 40)
-					socket.emit("movePad", { direction: "down", roomId: game.id, type: "local" });
+					socket.emit("movePad", {
+						direction: "down",
+						roomId: game.id,
+						type: "local",
+					});
 			}
 			if (game.state === "readyPlay" && event.key === "Enter") {
-				socket.emit('gameLoop', { roomId: game.id, state: "start" });
+				socket.emit("gameLoop", { roomId: game.id, state: "start" });
 			}
 		}
-	}
+	};
 
 	return (
 		<Layout>
-			<div tabIndex={0} className={classes.autoFocus} ref={autoFocusRef} onKeyDown={handleKeyDown}>
-				{game && game.state != "end" ? <GameCanvas className={classes.gameCanvas} game={game} /> : <p>There is a winner</p>}
+			<div
+				tabIndex={0}
+				className={classes.autoFocus}
+				ref={autoFocusRef}
+				onKeyDown={handleKeyDown}
+			>
+				{game && game.state != "end" ? (
+					<React.Fragment>
+						<GameCanvas className={classes.gameCanvas} game={game} />
+						<div className={classes.badges}>
+							<div className={classes.badge1}>
+								<img
+									src={
+										game.player1.user?.profileImage
+											? game.player1.user.profileImage
+											: game.player1.user?.image_url
+									}
+									alt="Profile Player1"
+									className={classes.profile}
+								/>
+								<div className={classes.username}>
+									{game.player1.user.username}
+								</div>
+							</div>
+							<div className={classes.badge2}>
+								<img
+									src={
+										game.player2.user?.profileImage
+											? game.player2.user.profileImage
+											: game.player2.user?.image_url
+									}
+									alt="Profile player 2"
+									className={classes.profile}
+								/>
+								<div className={classes.username}>
+									{game.player2.user.username}
+								</div>
+							</div>
+						</div>
+					</React.Fragment>
+				) : (
+					<p>There is a winner</p>
+				)}
 			</div>
 		</Layout>
-	)
-}
+	);
+};
 
 export default GamePage;
